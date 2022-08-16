74ae7b32271fd7972a52892abcb4a011f7dc2bd4
Amos Langat <amos.langat@student.moringaschool.com>
2022-08-16 17:37:16 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+let currentUser = "Amos";
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+let upperCaseCurrentUser = currentUser.toUpperCase();
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + upperCaseCurrentUser + "!";
+let initialCurrentUser = currentUser[0];
+let shortGreeting = "Welcome, " + initialCurrentUser + "!";
